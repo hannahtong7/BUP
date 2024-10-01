dc4ac756fc1d1f064e26a35cb06977066084e231
hannahtong7 <s2743332@ed.ac.uk>
2024-10-01 13:57:14 +0100

    Downloading climate data

--- Species Distribution Modelling/BUP - Species distribution modelling Workshop I.R
+++ Species Distribution Modelling/BUP - Species distribution modelling Workshop I.R
@@ -13,3 +13,10 @@ summary(avi_dat)
 ouzel_cols <- c('Turdus_torquatus', 'bio_5', 'bio_2', 'bio_14', 'blockCV_tile')
 ouzel_df <- data.frame(avi_dat)[ouzel_cols]
 summary(ouzel_df)
+
+#Import current and future climate data for Switzerland and clip it to the extent of Switzerland
+#download and import the same climate variables in directly from CLIMWIN.
+output_dir<-"~/Documents/temp/BUP/Species Distribution Modelling"
+options(timeout=2000)
+
+bio_curr <- getData('worldclim', var='bio', res=0.5, lon=5.5, lat=45.5, path=output_dir)[[c(2,5,14)]]
