a303cfc5b05128fa4c452661a349ea02f008a1b1
hannahtong7 <s2743332@ed.ac.uk>
2024-10-01 13:32:37 +0100

    Load data

---
+++ Workshop 1/BUP - Species distribution modelling Workshop I.R
@@ -0,0 +1,9 @@
+install.packages("raster",dependencies=TRUE,repos="https://cloud.r-project.org")
+library(raster)
+
+#Set working directory 
+setwd("~/Documents/temp")
+
+#Data preparation - download ring ouzel and climate data
+avi_dat <- read.table('Data_SwissBreedingBirds.csv', header=T, sep=',')
+head(avi_dat)
