f62b786c38800a904b7a8a40625aa0e149a53166
hannahtong7 <s2743332@ed.ac.uk>
2024-10-15 14:07:09 +0100

    Modify the function to include logistic growth

--- Multi-species interactions.R
+++ Multi-species interactions.R
@@ -51,7 +51,7 @@ state <- c(x = 10, y = 10)  # Initial prey (x) and predator (y) populations
 parameters <- c(alpha = 0.1, beta = 0.02, delta = 0.02, gamma = 0.4)
 
 # Define a sequence of time steps (500 steps with intervals of 0.01)
-times <- seq(0, 5, by = 0.01)  # from 0 to 5, in intervals of 0.01 (500 steps total)
+times <- seq(0, 500, by = 0.01)  # from 0 to 5, in intervals of 0.01 (500 steps total)
 
 # Solve the system of differential equations using the ode() function
 out <- ode(y = state, times = times, func = LV, parms = parameters)
@@ -61,8 +61,38 @@ out.df <- data.frame(out)
 
 # View the first few rows of the output
 head(out.df)
+#Plot the output
+#This gives the changes in the two populations through time. 
+library(ggplot2)
+ggplot(data = out.df)+
+  geom_line(mapping=aes(x=time,y=x),color="blue") +
+  geom_line(mapping=aes(x=time,y=y),color="red") +
+  geom_hline(yintercept=0,color="darkgrey") +
+  geom_vline(xintercept=0,color="darkgrey") +
+  labs(x = "Time", y = "P")
+
+ggplot(data = out.df)+
+  geom_path(mapping=aes(x=x,y=y),color="red") +
+  xlim(0,70) +
+  ylim(0,40) +
+  geom_hline(yintercept=0,color="darkgrey") +
+  geom_vline(xintercept=0,color="darkgrey") +
+  labs(x = "Prey", y = "Predator")
 
 
+#Prey growth rate: exponential vs logistic
+#In the original LV model, prey growth is exponential if no predators exist.
+#To add logistic growth to the prey population you modify the equation to:
+#dx/dt=alpha*x(1-x/K)-beta*x*y (K=carrying capacity)
+LV_logistic <- function(t, state, parameters) {
+  with(as.list(c(state, parameters)), {
+    
+    dx <- alpha * x * (1 - x / K) - beta * x * y  ## Prey logistic growth
+    dy <- delta * x * y - gamma * y               ## Predator equation
+    
+    return(list(c(dx, dy))) ## Return both rates of change
+  }) 
+}
 
 
 
